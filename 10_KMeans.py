f8efcfce05a13321a9fc4e5d7356a9e3a0c79336
aashishrn <aashishrn29@gmail.com>
2019-10-01 14:48:41 +0530

    Create 10_KMeans.py

---
+++ 10_KMeans.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Author: Ashish Ranjan
+Lang: Python3.7
+IDE: Spyder 3.3 (Anaconda3)
+Data: Randomly generated.
+
+K-Means applied on data generated in groups to guess the number of groups.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.datasets import make_blobs
+x, y = make_blobs(n_samples = 1000, centers = 5, cluster_std = 0.7)
+plt.scatter(x[:, 0], x[:, 1])
+plt.show()
+
+from sklearn.cluster import KMeans
+
+wcv = []
+
+for i in range(1, 16):
+    km = KMeans(n_clusters = i)
+    km.fit(x)
+    wcv.append(km.inertia_)
+
+plt.plot(range(1, 16), wcv)
+
+km = KMeans(n_clusters = 5)
+y_pred = km.fit_predict(x)
+
+plt.scatter(x[y_pred==0, 0], x[y_pred==0, 1], c = 'r')
+plt.scatter(x[y_pred==1, 0], x[y_pred==1, 1], c = 'b')
+plt.scatter(x[y_pred==2, 0], x[y_pred==2, 1], c = 'g')
+plt.scatter(x[y_pred==3, 0], x[y_pred==3, 1], c = 'y')
